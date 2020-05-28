33aff8541513fe552ea0e8d997556e73a2f9f1a2
Dennie Froese <dennie.froese@santander.co.uk>
2020-05-28 18:41:14 +0100

    updated board component to map over field object and push rows and columns ro render

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -3,14 +3,40 @@ import Cell from "../Cell";
 
 interface Props {
   field: any[];
+  gameOver?: boolean;
+  score?: number;
+  level?: number;
 }
 
-export default function Board({ field }: Props) {
+export default function Board({ field, gameOver, score, level }: Props) {
+  let rows: any[] = [];
+
+  field.forEach((row, index) => {
+    const cols = row.map((column: any, index: number) => (
+      <Cell className={`col-${column}`} key={index} />
+    ));
+
+    rows.push(
+      <div className="tetris-board__row" key={index}>
+        {cols}
+      </div>
+    );
+  });
+
   return (
-    <div>
-      {field.map(row =>
-        row.map((cell: any, x: number) => <Cell key={x} type={cell[0]} />)
-      )}
+    <div className="tetris-board">
+      <div className="tetris-board__info">
+        <p className="tetris-board__text">Level: {level}</p>
+
+        <p className="tetris-board__text">Score: {score}</p>
+
+        {gameOver && (
+          <p className="tetris-board__text">
+            <strong>Game Over</strong>
+          </p>
+        )}
+      </div>
+      <div className="tetris-board__board">{rows}</div>
     </div>
   );
 }
