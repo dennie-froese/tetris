d1fd74e4945cd26c113e9fbfc67dba3c3bd41c82
Dennie Froese <dennie.froese@santander.co.uk>
2020-05-29 19:17:16 +0100

    updated and typed tiles

--- src/tiles.ts
+++ src/tiles.ts
@@ -1,99 +1,219 @@
-export type TyleZero = {
-  shape: [[number]];
-  color: string;
-};
-export type TyleTwo = {
-  shape: [
-    [number | string, number | string],
-    [number | string, number | string]
-  ];
-  color: string;
-};
-export type TyleThree = {
-  shape: [
-    [number | string, number | string, number | string],
-    [number | string, number | string, number | string],
-    [number | string, number | string, number | string]
-  ];
-  color: string;
-};
-export type TyleFour = {
-  shape: [
-    [number | string, number | string, number | string, number | string],
-    [number | string, number | string, number | string, number | string],
-    [number | string, number | string, number | string, number | string],
-    [number | string, number | string, number | string, number | string]
-  ];
-  color: string;
-};
-
-export type Tyles = {
-  0: TyleZero;
-  I: TyleFour;
-  J: TyleThree;
-  L: TyleThree;
-  O: TyleTwo;
-  S: TyleThree;
-  T: TyleThree;
-  Z: TyleThree;
-};
-
-export const tiles: Tyles = {
-  0: { shape: [[0]], color: "0,0,0" },
-  I: {
-    shape: [
-      [0, "I", 0, 0],
-      [0, "I", 0, 0],
-      [0, "I", 0, 0],
-      [0, "I", 0, 0]
-    ],
-    color: "90, 227, 230"
-  },
-  J: {
-    shape: [
-      [0, "J", 0],
-      [0, "J", 0],
-      ["J", "J", 0]
-    ],
-    color: "36, 95, 223"
-  },
-  L: {
-    shape: [
-      [0, "L", 0],
-      [0, "L", 0],
-      [0, "L", "L"]
-    ],
-    color: "223, 173, 36"
-  },
-  O: {
-    shape: [
+export const tiles: number[][][][] = [
+  // Each tile can be rotated 4 times (x/y coordinates)
+  [
+    // square
+    [
+      [0, 0],
+      [0, 0],
       [0, 0],
       [0, 0]
     ],
-    color: "223, 217, 36"
-  },
-  S: {
-    shape: [
-      [0, "S", "S"],
-      ["S", "S", 0],
-      [0, 0, 0]
+    [
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0]
     ],
-    color: "48, 211, 56"
-  },
-  T: {
-    shape: [
-      [0, 0, 0],
-      ["T", "T", "T"],
-      [0, "T", 0]
+    [
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0]
     ],
-    color: "132, 61, 198"
-  },
-  Z: {
-    shape: [
-      ["Z", "Z", 0],
-      [0, "Z", "Z"],
-      [0, 0, 0]
+    [
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0]
+    ]
+  ],
+  [
+    // cube tile (block 2x2)
+    [
+      [0, 0],
+      [1, 0],
+      [0, 1],
+      [1, 1]
     ],
-    color: "227, 78, 78"
-  }
-};
+    [
+      [0, 0],
+      [1, 0],
+      [0, 1],
+      [1, 1]
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [0, 1],
+      [1, 1]
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [0, 1],
+      [1, 1]
+    ]
+  ],
+  [
+    // I
+    [
+      [0, -1],
+      [0, 0],
+      [0, 1],
+      [0, 2]
+    ],
+    [
+      [-1, 0],
+      [0, 0],
+      [1, 0],
+      [2, 0]
+    ],
+    [
+      [0, -1],
+      [0, 0],
+      [0, 1],
+      [0, 2]
+    ],
+    [
+      [-1, 0],
+      [0, 0],
+      [1, 0],
+      [2, 0]
+    ]
+  ],
+  [
+    //  T
+    [
+      [0, 0],
+      [-1, 0],
+      [1, 0],
+      [0, -1]
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [0, 1],
+      [0, -1]
+    ],
+    [
+      [0, 0],
+      [-1, 0],
+      [1, 0],
+      [0, 1]
+    ],
+    [
+      [0, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1]
+    ]
+  ],
+  [
+    // inverse L tile
+    [
+      [0, 0],
+      [-1, 0],
+      [1, 0],
+      [-1, -1]
+    ],
+    [
+      [0, 0],
+      [0, 1],
+      [0, -1],
+      [1, -1]
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [-1, 0],
+      [1, 1]
+    ],
+    [
+      [0, 0],
+      [0, 1],
+      [0, -1],
+      [-1, 1]
+    ]
+  ],
+  [
+    // L
+    [
+      [0, 0],
+      [1, 0],
+      [-1, 0],
+      [1, -1]
+    ],
+    [
+      [0, 0],
+      [0, 1],
+      [0, -1],
+      [1, 1]
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [-1, 0],
+      [-1, 1]
+    ],
+    [
+      [0, 0],
+      [0, 1],
+      [0, -1],
+      [-1, -1]
+    ]
+  ],
+  [
+    // Z
+    [
+      [0, 0],
+      [1, 0],
+      [0, -1],
+      [-1, -1]
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [0, 1],
+      [1, -1]
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [0, -1],
+      [-1, -1]
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [0, 1],
+      [1, -1]
+    ]
+  ],
+  [
+    // inverse Z tile
+    [
+      [0, 0],
+      [-1, 0],
+      [0, -1],
+      [1, -1]
+    ],
+    [
+      [0, 0],
+      [0, -1],
+      [1, 0],
+      [1, 1]
+    ],
+    [
+      [0, 0],
+      [-1, 0],
+      [0, -1],
+      [1, -1]
+    ],
+    [
+      [0, 0],
+      [0, -1],
+      [1, 0],
+      [1, 1]
+    ]
+  ]
+];
