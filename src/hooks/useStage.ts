4e0d319fb510ea0914fe62d329d71925e5af2b15
Dennie Froese <dennie.froese@santander.co.uk>
2020-06-01 21:09:22 +0100

    completed useStage hook

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -1,1 +1,54 @@
 import { useState, useEffect } from "react";
+import { createBoard } from "../gameHelpers";
+
+export const useBoard = (player, resetPlayer) => {
+  const [board, setBoard] = useState(createBoard);
+  const [rowsCleared, setRowsCleared] = useState(0);
+
+  useEffect(() => {
+    setRowsCleared(0);
+    const sweepRows = newBoard =>
+      newBoard.reduce((ack, row) => {
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1);
+          ack.unshift(new Array(newBoard[0].length).fill([0, "clear"]));
+          return ack;
+        }
+        ack.push(row);
+        return ack;
+      }, []);
+
+    const updateBoard = prevBoard => {
+      const newBoard = prevBoard.map(row =>
+        row.map(cell => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+
+      player.tile.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newBoard[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? "merged" : "clear"}`
+            ];
+          }
+        });
+      });
+
+      if (player.collided) {
+        resetPlayer();
+        return sweepRows(newBoard);
+      }
+      return newBoard;
+    };
+
+    setBoard(prev => updateBoard(prev));
+  }, [
+    player.collided,
+    player.pos.x,
+    player.pos.y,
+    player.tetromino,
+    resetPlayer
+  ]);
+
+  return [board, setBoard, rowsCleared];
+};
