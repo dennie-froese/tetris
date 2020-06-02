46a3b88d3d7e2fbef3e7eaa352153e919779ff2b
Dennie Froese <dennie.froese@santander.co.uk>
2020-06-02 08:20:55 +0100

    added player state object to usePlayer hook

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -1,3 +1,12 @@
 import { useState, useCallback } from "react";
 
 import { tiles, randomTile } from "../tiles";
+import { BOARD_HEIGHT, checkCollision } from "../gameHelpers";
+
+export const usePlayer = () => {
+  const [player, setPlayer] = useState({
+    pos: { x: 0, y: 0 },
+    tile: tiles[0].shape,
+    collided: false
+  });
+};
