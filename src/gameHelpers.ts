57c34ea3e1cabc005078a3df96fdcbe7bf47bd82
Dennie Froese <dennie.froese@santander.co.uk>
2020-06-02 08:17:34 +0100

    added checkCollision func in gamehelpers

--- src/gameHelpers.ts
+++ src/gameHelpers.ts
@@ -3,3 +3,25 @@ export const BOARD_HEIGHT = 20;
 
 export const createBoard = () =>
   Array.from(Array(BOARD_HEIGHT), () => Array(BOARD_HEIGHT).fill([0, "clear"]));
+
+export const checkCollision = (
+  player: any,
+  board: any,
+  { x: moveX, y: moveY }: any
+) => {
+  for (let y = 0; y < player.tile.length; y += 1) {
+    for (let x = 0; x < player.tile[y].length; x += 1) {
+      if (player.tile[y][x] !== 0) {
+        if (
+          !board[y + player.pos.y + moveY] ||
+          !board[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          board[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+            "clear"
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+};
