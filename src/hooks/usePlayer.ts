6d65c9dd583c8641b6d1e8737d125ad3c56abed2
Dennie Froese <dennie.froese@santander.co.uk>
2020-06-02 08:40:38 +0100

    added rotate and playerRotate functions to usePlayer hook

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -4,9 +4,14 @@ import { tiles, randomTile, TilesShapes } from "../tiles";
 import { BOARD_HEIGHT, checkCollision } from "../gameHelpers";
 
 type Keys = "pos" | "tile" | "collided";
+type KeysPosition = "y" | "x";
+
+type PosObject = {
+  [k in KeysPosition]: number;
+};
 
 export type Player = {
-  [k in Keys]: { x: number; y: number } | TilesShapes | boolean;
+  [k in Keys]: PosObject | TilesShapes | boolean;
 };
 
 export const usePlayer = () => {
@@ -15,4 +20,30 @@ export const usePlayer = () => {
     tile: tiles[0].shape,
     collided: false
   });
+
+  function rotate(matrix: any, dir: number) {
+    const mtrx = matrix.map((_: any, index: number) =>
+      matrix.map((column: any) => column[index])
+    );
+    if (dir > 0) return mtrx.map((row: any) => row.reverse());
+    return mtrx.reverse();
+  }
+
+  function playerRotate(board: any, dir: number) {
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tile = rotate(clonedPlayer.tile, dir);
+
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, board, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tile[0].length) {
+        rotate(clonedPlayer.tile, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+    setPlayer(clonedPlayer);
+  }
 };
