9501efac5b91b5267956c6e0af5370099b1b0d88
Dennie Froese <dennie.froese@santander.co.uk>
2020-06-08 17:39:12 +0100

    minor changes to types in board component and handing down new keyVal prop to cells

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -4,37 +4,22 @@ import { BOARD_HEIGHT, BOARD_WIDTH } from "../../gameHelpers";
 
 export default function Board({ board }: any) {
   return (
-    <div className="tetris-board">
-      {/* <div className="tetris-board__info">
-        <p className="tetris-board__text">Level: {level}</p>
-
-        <p className="tetris-board__text">Score: {score}</p>
-
-        {gameOver && (
-          <p className="tetris-board__text">
-            <strong>Game Over</strong>
-          </p>
-        )}
-      </div> */}
-      <div
-        className="tetris-board__board"
-        style={{
-          display: "grid",
-          gridTemplateRows: `repeat(
+    <div
+      style={{
+        display: "grid",
+        gridTemplateRows: `repeat(
             ${BOARD_HEIGHT},
             calc(25vw / ${BOARD_WIDTH})
           )`,
-          gridTemplateColumns: `repeat(${BOARD_WIDTH}, 1fr)`,
-          gridGap: "1px",
-          border: "2px solid ",
-          width: "100%",
-          background: "#111"
-        }}
-      >
-        {board.map((row: any) =>
-          row.map((cell: any, x: number) => <Cell key={x} type={cell[0]} />)
-        )}
-      </div>
+        gridTemplateColumns: `repeat(${BOARD_WIDTH}, 1fr)`,
+        border: "2px solid ",
+        width: "100%",
+        background: "#111"
+      }}
+    >
+      {board.map((row: [][]) =>
+        row.map((cell: any, x: number) => <Cell keyVal={x} type={cell[0]} />)
+      )}
     </div>
   );
 }
